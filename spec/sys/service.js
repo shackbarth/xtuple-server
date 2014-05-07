96f660f7c06723f27f654938cc9eb5c6b3665951
Travis Webb <me@traviswebb.com>
2014-05-06 20:18:10 -0400

    fix uninstall, updated service tests

--- spec/sys/service.js
+++ spec/sys/service.js
@@ -13,3 +13,19 @@ it('all pm2 services accounted for (server, healthfeed, snapshotmgr)', function 
   assert.lengthOf(pm2config, 3);
 });
 
+describe('service xtuple <version> status (pm2 jlist)', function () {
+  it('xtuple web server is running', function () {
+    var jlist = JSON.parse(exec('pm2 jlist').stdout),
+      serverProcessName = 'xtuple-server-' + options.xt.version + '-' + options.xt.name,
+      serverProcess = _.findWhere(jlist.processes, { name: serverProcessName });
+
+    assert.equal(serverProcess.status, 'online', 'xTuple web server is down');
+  });
+  it('health monitor is running', function () {
+    var jlist = JSON.parse(exec('pm2 jlist').stdout),
+      healthProcessName = 'xtuple-healthfeed-' + options.xt.version + '-' + options.xt.name,
+      healthProcess = _.findWhere(jlist.processes, { name: healthProcessName });
+
+    assert.equal(healthProcess.status, 'online', 'xTuple Health Feed is down');
+  });
+});
