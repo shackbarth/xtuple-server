5d910e278ae8e1898ad1e45e2972efd766a417b2
Travis Webb <me@traviswebb.com>
2014-05-07 02:29:47 -0400

    webmin improvements + plan options
    - added ability to define default options in the planfile
    - create webmin command html description files
    - configure webmin referrer settings

--- tasks/sys/webmin/index.js
+++ tasks/sys/webmin/index.js
@@ -6,11 +6,11 @@
    */
   var webmin = exports;
 
-  var lib = require('../../lib'),
+  var lib = require('../../../lib'),
     format = require('string-format'),
     _ = require('lodash'),
     path = require('path'),
-    mkdirp = require('mkdirp'),
+    mkdirp = require('mkdirp').sync,
     exec = require('execSync').exec,
     fs = require('fs');
 
@@ -18,25 +18,35 @@
 
     /** @override */
     beforeInstall: function (options) {
-      options.sys.webminConfigPath = path.resolve('/etc/webmin');
-      options.sys.webminCustomPath = path.resolve(options.sys.webminConfigPath, 'custom');
-      options.sys.webminXtuplePath = path.resolve(options.sys.webminConfigPath, 'xtuple');
+      mkdirp('/srv/ssl');
+
+      options.nginx || (options.nginx = { });
+      options.nginx.outkey = path.resolve('/srv/ssl/xtremote.key');
+      options.nginx.outcrt = path.resolve('/srv/ssl/xtremote.crt');
+      options.nginx.domain = 'localhost';
+
+      options.sys.etcWebmin = path.resolve('/etc/webmin');
+      options.sys.webminConfigFile = path.resolve(options.sys.etcWebmin, 'config');
+      options.sys.webminCustomPath = path.resolve(options.sys.etcWebmin, 'custom');
+      options.sys.webminCustomConfigFile = path.resolve(options.sys.webminCustomPath, 'config');
+      options.sys.webminXtuplePath = path.resolve(options.sys.etcWebmin, 'xtuple');
     },
 
     /** @override */
     beforeTask: function (options) {
-      mkdirp(options.nginx.webminXtuplePath);
+      mkdirp(options.sys.webminXtuplePath);
     },
 
     /** @override */
-    doTask: function (options) {
+    executeTask: function (options) {
       var debFile = 'webmin_1.680_all.deb';
       // TODO if debian
       if (!fs.existsSync(path.resolve(debFile))) {
         exec('wget https://s3.amazonaws.com/com.xtuple.deploy-assets/'+ debFile);
       }
-      exec('dpkg --install '+ debFile);
+      //exec('dpkg --install '+ debFile);
 
+      webmin.writeConfiguration(options);
       webmin.installCustomCommands(options);
       webmin.installNginxSite(options);
     },
@@ -44,37 +54,65 @@
     /** @override */
     afterTask: function (options) {
       exec('service nginx reload');
+      exec('service webmin restart');
+    },
+
+    writeConfiguration: function (options) {
+      fs.appendFileSync(options.sys.webminConfigFile, [
+        'webprefix=/_manage',
+        'webprefixnoredir=1',
+        'referer=1'
+      ].join('\n').trim());
+
+      fs.writeFileSync(options.sys.webminCustomConfigFile, [
+        'display_mode=1',
+        'columns=1',
+        'params_cmd=0',
+        'params_file=0',
+        'sort=desc',
+        'height=',
+        'width=',
+        'wrap='
+      ].join('\n').trim());
     },
 
     installCustomCommands: function (options) {
+      fs.unlinkSync(path.resolve(options.sys.webminXtuplePath, 'editions.menu'));
+      fs.unlinkSync(path.resolve(options.sys.webminCustomPath, '1001.cmd'));
+      fs.unlinkSync(path.resolve(options.sys.webminCustomPath, '1002.cmd'));
+
       fs.writeFileSync(
         path.resolve(options.sys.webminXtuplePath, 'editions.menu'),
         fs.readFileSync(path.resolve(__dirname, 'editions.menu'))
       );
       fs.writeFileSync(
         path.resolve(options.sys.webminCustomPath, '1001.cmd'),
-        fs.readFileSync(path.resolve(__dirname, 'server-install-file.cmd'))
+        fs.readFileSync(path.resolve(__dirname, 'server-install-file.cmd').toString().format(options))
       );
       fs.writeFileSync(
         path.resolve(options.sys.webminCustomPath, '1002.cmd'),
-        fs.readFileSync(path.resolve(__dirname, 'server-install-upload.cmd'))
+        fs.readFileSync(path.resolve(__dirname, 'server-install-upload.cmd').toString().format(options))
+      );
+      fs.writeFileSync(
+        path.resolve(options.sys.webminCustomPath, '1001.html'),
+        fs.readFileSync(path.resolve(__dirname, 'server-install-file.html').toString().format(options))
+      );
+      fs.writeFileSync(
+        path.resolve(options.sys.webminCustomPath, '1002.html'),
+        fs.readFileSync(path.resolve(__dirname, 'server-install-upload.html').toString().format(options))
       );
     },
 
     installNginxSite: function (options) {
-      options.nginx || (options.nginx = { });
-      options.nginx.outkey = path.resolve('/srv/ssl/xtremote.key');
-      options.nginx.outcrt = path.resolve('/srv/ssl/xtremote.crt');
-
       if (!fs.existsSync(options.nginx.outcrt)) {
-        require('../nginx/ssl').generate(options);
+        require('../../nginx').ssl.generate(options);
       }
 
       // write site file
       options.nginx.availableSite = path.resolve('/etc/nginx/sites-available/webmin-site');
       options.nginx.enabledSite = path.resolve('/etc/nginx/sites-enabled/webmin-site');
       options.nginx.siteTemplateFile = path.resolve(__dirname, 'webmin-site');
-      require('../nginx/site').writeSiteConfig(options);
+      require('../../nginx').site.writeSiteConfig(options);
     }
   });
 
