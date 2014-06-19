2dc9121f079780314a3fdae3fafd33be0e5bd358
Travis Webb <me@traviswebb.com>
2014-06-19 12:05:03 -0400

    dump commit

---
+++ link-core.js
@@ -0,0 +1,42 @@
+var _ = require('lodash'),
+  path = require('path'),
+  glob = require('glob'),
+  fs = require('fs');
+
+process.chdir(__dirname);
+
+var workspace = require('./workspace');
+
+// target = me
+_.each(workspace.linkImports, function (_target, _source) {
+  var split = _source.split(path.sep);
+  var target = path.resolve(_target);
+  var files = glob.sync(
+    path.resolve(path.dirname(require.resolve(split[0])), split.slice(1).join(path.sep))
+  );
+
+  _.each(files, function (file) {
+    var targetfile = path.resolve(target, path.basename(file));
+
+    if (!fs.existsSync(targetfile)) {
+      fs.symlinkSync(file, targetfile);
+    }
+  });
+  console.log('linkImports: done');
+});
+
+// source = me
+_.each(workspace.linkExports, function (_target, _source) {
+  var split = _target.split(path.sep);
+  var target = path.resolve(path.dirname(require.resolve(split[0])), split.slice(1).join(path.sep));
+  var files = glob.sync(path.resolve(_source));
+
+  _.each(files, function (file) {
+    var targetfile = path.resolve(target, path.basename(file));
+
+    if (!fs.lstatSync(file).isSymbolicLink() && !fs.existsSync(targetfile)) {
+      fs.symlinkSync(file, targetfile);
+    }
+  });
+  console.log('linkExports: done');
+});
--- spec/index.js
+++ spec/index.js
@@ -1,9 +1,17 @@
-var x = describe('(v'+ require('../package').version + ') xtuple-server-commercial', function () {
+describe('xtuple-server-commercial', function () {
+
+  describe('Core', function (done) {
+    this.timeout(1800 * 1000); // 30 minutes
+    require('xtuple-server/spec');
+  });
+
   describe('sanity', function () {
 
   });
+
   describe('plans', function () {
     require('./uninstall-live');
     require('./install-live');
   });
+
 });
