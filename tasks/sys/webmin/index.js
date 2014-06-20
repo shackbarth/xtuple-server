723637f810bf231f1b650f72427386d473e7d5f4
Travis Webb <me@traviswebb.com>
2014-06-20 07:14:31 -0400

    added deps to webmin

--- tasks/sys/webmin/index.js
+++ tasks/sys/webmin/index.js
@@ -109,14 +109,14 @@
 
     installNginxSite: function (options) {
       if (!fs.existsSync(options.nginx.outcrt)) {
-        require('../../nginx').ssl.generate(options);
+        require('xtuple-server-nginx-ssl').generate(options);
       }
 
       // write site file
       options.nginx.availableSite = path.resolve('/etc/nginx/sites-available/webmin-site');
       options.nginx.enabledSite = path.resolve('/etc/nginx/sites-enabled/webmin-site');
       options.nginx.siteTemplateFile = path.resolve(__dirname, 'webmin-site');
-      require('../../nginx').site.writeSiteConfig(options);
+      require('xtuple-server-nginx-site').writeSiteConfig(options);
     },
 
     deleteUnusedModules: function (options) {
