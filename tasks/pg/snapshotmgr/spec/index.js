d1b5e0e0afcfca838b15ba05e4a4cd160e5baa34
Travis Webb <me@traviswebb.com>
2014-06-22 22:08:54 -0400

    fix crontab directory typo

--- tasks/pg/snapshotmgr/spec/index.js
+++ tasks/pg/snapshotmgr/spec/index.js
@@ -7,7 +7,7 @@ exports.afterExecute = function (options) {
   describe('crontab', function () {
 
     it('should contain cron entry in crontab file', function () {
-      var content = fs.readFileSync(path.resolve('/var/pool/cron/crontabs', options.xt.name)).toString();
+      var content = fs.readFileSync(path.resolve('/var/spool/cron/crontabs', options.xt.name)).toString();
       console.log(content);
     });
 
