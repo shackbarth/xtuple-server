1d0baf384580a52558d08c8f805f5c1568dac61c
Travis Webb <me@traviswebb.com>
2014-06-27 13:23:54 -0400

    ci nde thrash

--- link-core.js
+++ link-core.js
@@ -1,6 +1,7 @@
 var _ = require('lodash'),
   path = require('path'),
   glob = require('glob'),
+  log = require('npmlog'),
   fs = require('fs');
 
 process.chdir(__dirname);
@@ -22,7 +23,7 @@ _.each(workspace.linkImports, function (_target, _source) {
       fs.symlinkSync(file, targetfile);
     }
   });
-  console.log('linkImports: done');
+  log.info('nde', 'linkImports', 'done');
 });
 
 // source = me
@@ -36,10 +37,10 @@ _.each(workspace.linkExports, function (_target, _source) {
   _.each(files, function (file) {
     var targetfile = path.resolve(target, path.basename(file));
 
-    if (/*!fs.lstatSync(file).isSymbolicLink() && */!fs.existsSync(targetfile)) {
-      console.log(targetfile);
+    if (!fs.existsSync(targetfile) && fs.existsSync(file)) {
+      log.info('nde', 'installing '+ file + ' -> ' + targetfile);
       fs.symlinkSync(file, targetfile);
     }
   });
 });
-console.log('linkExports: done');
+log.info('nde', 'linkExports', 'done');
