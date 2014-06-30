05a4364a3d750ef41981a7394f5dcdd792ac3b3b
Travis Webb <me@traviswebb.com>
2014-06-30 05:41:17 -0400

    updated nex symlinkExport

--- link-core.js
+++ link-core.js
@@ -4,25 +4,25 @@ var _ = require('lodash'),
   log = require('npmlog'),
   fs = require('fs');
 
+log.heading = 'nex';
+
 process.chdir(__dirname);
 
-var workspace = require('./workspace');
 var pkg = require('./package');
 
-_.each(workspace.linkExports, function (_target, _source) {
+_.each(pkg.symlinkExports, function (_target, _source) {
   //var split = _target.split(path.sep);
   //var target = path.resolve(path.dirname(require.resolve(split[0])), split.slice(1).join(path.sep));
   var target = path.resolve(_target);
   var files = glob.sync(path.resolve(_source));
-  console.log('target dir: '+ target);
 
   _.each(files, function (file) {
     var targetfile = path.resolve(target, path.basename(file));
 
     if (!fs.existsSync(targetfile) && fs.existsSync(file)) {
-      log.info('nex', 'installing '+ file + ' -> ' + targetfile);
+      log.info('symlinkExports', file, '->', targetfile);
       fs.symlinkSync(file, targetfile);
     }
   });
 });
-log.info('nex', 'linkExports', 'done');
+log.info('symlinkExports', 'done.');
