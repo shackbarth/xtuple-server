6c95391148ee14d4c699590b4e609f8fe3a35bbb
Travis Webb <me@traviswebb.com>
2014-07-01 08:52:00 -0400

    created import-users plan and webmin command

---
+++ tasks/sys/webmin/import-users.cmd
@@ -0,0 +1,9 @@
+xtuple-server import-users $instanceType --xt-name $xtName --xt-version $xtVersion --pg-infile $pgInfile --pg-dbname $pgDbname
+Restore Database (xtuple-server restore-dataase)
+root 0 0 0 0 1 3600 0 -
+xtName:0::1,1:Account Name
+xtVersion:0:4.5.0:1,1:xTuple App Version
+instanceType:9:/etc/webmin/xtuple/types.menu:1,1:Instance Type
+pgInfile:5::1,1:Globals Backup File (.sql)
+pgDbname:0::1,1:Database Name
+
