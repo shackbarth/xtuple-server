d86a1e579d2f184ad0ce7fa5d805cae4ef468c33
Travis Webb <me@traviswebb.com>
2014-07-02 01:28:13 -0400

    new file:   databases.menu   - created database multi select menu (demo, empty, quickstart) modified:   install.cmd   - added database menu to install command

--- tasks/sys/webmin/install.cmd
+++ tasks/sys/webmin/install.cmd
@@ -1,13 +1,11 @@
-xtuple-server install-$instanceType --xt-name $xtName --xt-version $xtVersion --xt-edition $xtEdition $xtDemo $xtQuickstart $xtEmpty --xt-maindb $xtMaindb --xt-adminpw $xtAdminpw --nginx-domain $nginxDomain --nginx-inkey $nginxInkey --nginx-incrt $nginxIncrt --nginx-inzip $nginxInzip --pg-slots $pgSlots --pg-capacity $pgCapacity $pgEnableSnapshots
+xtuple-server install-$instanceType --xt-name $xtName --xt-version $xtVersion --xt-edition $xtEdition $xtDatabases --xt-maindb $xtMaindb --xt-adminpw $xtAdminpw --nginx-domain $nginxDomain --nginx-inkey $nginxInkey --nginx-incrt $nginxIncrt --nginx-inzip $nginxInzip --pg-slots $pgSlots --pg-capacity $pgCapacity $pgEnableSnapshots
 New xTuple Deployment (xtuple-server install)
 root 0 0 0 0 1 3600 0 -
 xtName:0::1,1:Account Name
 xtVersion:0:4.5.0:1,1:xTuple App Version
 instanceType:9:/etc/webmin/xtuple/types.menu:1,1:Instance Type
 xtEdition:9:/etc/webmin/xtuple/editions.menu:1,1:xTuple Edition
-xtDemo:7:--xt-demo:0,0:Install Demo?
-xtQuickstart:7:--xt-quickstart:0,0:Install Quickstart?
-xtEmpty:7:--xt-empty:0,0:Install Empty?
+xtDatabases:12:/etc/webmin/xtuple/databases.menu:0,1:Initialize Databases
 xtMaindb:5::1,0:xTuple Main Database File
 xtAdminpw:8::1,0:xTuple "admin" Password
 nginxDomain:0::1,0:Domain Name
