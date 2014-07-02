139c8768bde877684cc4456bd0693e1b9ab2f3ec
Travis Webb <me@traviswebb.com>
2014-07-02 01:43:38 -0400

    modified:   install.cmd   - added more postgres config options   - added snapshot options   - added pg version selector menu new file:   pgversions.menu   - menu for selecting pg version

--- tasks/sys/webmin/install.cmd
+++ tasks/sys/webmin/install.cmd
@@ -1,4 +1,4 @@
-xtuple-server install-$instanceType --xt-name $xtName --xt-version $xtVersion --xt-edition $xtEdition $xtDatabases --xt-maindb $xtMaindb --xt-adminpw $xtAdminpw --nginx-domain $nginxDomain --nginx-inkey $nginxInkey --nginx-incrt $nginxIncrt --pg-slots $pgSlots --pg-capacity $pgCapacity $pgEnableSnapshots
+xtuple-server install-$instanceType --xt-name $xtName --xt-version $xtVersion --xt-edition $xtEdition $xtDatabases --xt-maindb $xtMaindb --xt-adminpw $xtAdminpw --nginx-domain $nginxDomain --nginx-inkey $nginxInkey --nginx-incrt $nginxIncrt --pg-slots $pgSlots --pg-capacity $pgCapacity $pgEnableSnapshots $pgWorldLogin $pgSnapSchedule $pgSnapCount $pgPort $pgVersion
 New xTuple Deployment (xtuple-server install)
 root 0 0 0 0 1 3600 0 -
 xtName:0::1,1:Account Name
@@ -11,6 +11,11 @@ xtAdminpw:8::1,0:xTuple "admin" Password
 nginxDomain:0::1,0:Domain Name
 nginxInkey:5::1,0:SSL key (.key)
 nginxIncrt:5::1,0:SSL certificate (.crt or .pem)
-pgCapacity:0:48:1,0:Capacity (slots)
-pgSlots:0:1:1,0:Provision
+pgVersion:0:/etc/webmin/xtuple/pgversions.menu:1,1:Postgres Server Version
+pgCapacity:0:32:1,1:Capacity (slots)
+pgSlots:0:1:1,1:Provision (slots)
+pgWorldLogin:7:--pg-worldlogin:0,0:Expose Postgres to All IPs
+pgPort:0::1,0:Postgres Port
 pgEnableSnapshots:7:--pg-snapenable:0,0:Enable Snapshot Manager?
+pgSnapSchedule:0:@daily:1,1:Snapshot Schedule
+pgSnapCount:0:7:1,1:Snapshots to Retain
