3637fe7d2f939e426f3790cf0dd8a0180491497c
Travis Webb <me@traviswebb.com>
2014-07-02 01:29:55 -0400

    modified:   tasks/sys/webmin/install.cmd   - removed old nginx-inzip option

--- tasks/sys/webmin/install.cmd
+++ tasks/sys/webmin/install.cmd
@@ -1,4 +1,4 @@
-xtuple-server install-$instanceType --xt-name $xtName --xt-version $xtVersion --xt-edition $xtEdition $xtDatabases --xt-maindb $xtMaindb --xt-adminpw $xtAdminpw --nginx-domain $nginxDomain --nginx-inkey $nginxInkey --nginx-incrt $nginxIncrt --nginx-inzip $nginxInzip --pg-slots $pgSlots --pg-capacity $pgCapacity $pgEnableSnapshots
+xtuple-server install-$instanceType --xt-name $xtName --xt-version $xtVersion --xt-edition $xtEdition $xtDatabases --xt-maindb $xtMaindb --xt-adminpw $xtAdminpw --nginx-domain $nginxDomain --nginx-inkey $nginxInkey --nginx-incrt $nginxIncrt --pg-slots $pgSlots --pg-capacity $pgCapacity $pgEnableSnapshots
 New xTuple Deployment (xtuple-server install)
 root 0 0 0 0 1 3600 0 -
 xtName:0::1,1:Account Name
@@ -11,7 +11,6 @@ xtAdminpw:8::1,0:xTuple "admin" Password
 nginxDomain:0::1,0:Domain Name
 nginxInkey:5::1,0:SSL key (.key)
 nginxIncrt:5::1,0:SSL certificate (.crt or .pem)
-nginxInzip:5::1,0:SSL Bundle (.zip)
 pgCapacity:0:48:1,0:Capacity (slots)
 pgSlots:0:1:1,0:Provision
 pgEnableSnapshots:7:--pg-snapenable:0,0:Enable Snapshot Manager?
