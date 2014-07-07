c7d29ecfbb1e1fbdb867fa06e3341f99d733596a
Travis Webb <me@traviswebb.com>
2014-07-07 02:36:10 -0400

    use __dirname instead of cwd

--- install.js
+++ install.js
@@ -15,15 +15,12 @@ github.getRelease(repository)
   .then(function (tarball) {
     log.info('tarball', 'extracting', tarball);
 
-    process.chdir(path.resolve(process.cwd(), 'node_modules', pkg.name));
-    log.info('this file directory', __dirname);
-    log.info('cwd', process.cwd());
-    new targz().extract(tarball, path.resolve(process.cwd(), 'extract'), function (err) {
+    new targz().extract(tarball, path.resolve(__dirname, 'extract'), function (err) {
       if (err) return log.error('extract', err);
 
-      log.info('copying', path.resolve('extract', packageName, '*'), 'to', process.cwd());
+      log.info('copying', path.resolve('extract', packageName, '*'), 'to', __dirname);
 
-      proc.execSync([ 'cp -r', path.resolve('extract', packageName, '*'), process.cwd() ].join(' '));
+      proc.execSync([ 'cp -r', path.resolve('extract', packageName, '*'), __dirname ].join(' '));
     });
   },
   function () {
