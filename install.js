07ee50d33fbf172c9e614d31664920bfa436a935
Travis Webb <me@traviswebb.com>
2014-07-07 02:11:17 -0400

    create npm install/extract script

--- install.js
+++ install.js
@@ -1,18 +1,26 @@
+var targz = require('tar.gz');
 var path = require('path');
 var cp = require('cp');
-var proc = require('child_process').execSync;
+var proc = require('child_process');
 var log = require('npmlog');
 var github = require('nex-github');
 var pkg = require('./package');
 var repository = pkg.repository;
+var packageName = pkg.name + '-' + pkg.version;
 
 log.heading = 'nex-github';
 repository.version = pkg.version;
 
 github.getRelease(repository)
   .then(function (tarball) {
-    cp.sync(tarball, path.resolve(process.cwd()));
-    proc.exec('npm tarball-install '+ tarball);
+    log.info('tarball', 'extracting', tarball);
+
+    process.chdir(__dirname);
+    new targz().extract(tarball, 'extract', function (err) {
+      if (err) return log.error('extract', err);
+
+      proc.execSync([ 'cp -r', path.resolve('extract', packageName, '*'), '.' ].join(' '));
+    });
   },
   function () {
     log.error('Failed to download xtuple-server-commercial. Please try again');
