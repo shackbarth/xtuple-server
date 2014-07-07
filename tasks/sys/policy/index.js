2f35620cfbc3111f96fdc13b529d140a738480aa
Travis Webb <me@traviswebb.com>
2014-07-07 15:51:20 -0400

    proactively create ~/.git folder

--- tasks/sys/policy/index.js
+++ tasks/sys/policy/index.js
@@ -160,10 +160,12 @@ _.extend(exports, lib.task, /** @exports xtuple-server-sys-policy */ {
   copyGitCredentials: function (options) {
     var creds = path.resolve(home(), '.git-credentials');
     var rootCreds = path.resolve('/root', '.git-credentials');
+    mkdirp.sync(path.resolve(home(), '.git'));
     exec('git config credential.helper store');
     exec('sudo git config --global credential.helper "cache --timeout=31557600"');
 
     if (fs.existsSync(creds) && !fs.existsSync(rootCreds)) {
+      mkdirp.sync(path.resolve('/root', '.git'));
       cp.sync(creds, rootCreds);
       fs.chownSync(rootCreds, 0, 0);
       cp.sync(path.resolve(home(), '.gitconfig'), '/root/.gitconfig');
