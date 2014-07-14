227e4c97f0c01ed1b22358469441a7c0c8915444
Travis Webb <me@traviswebb.com>
2014-07-14 02:47:50 -0400

    added ghuser/ghpass to webmin install cmd
    - resolves #71
    - resolves #72
    
    modified:   tasks/sys/webmin/install.cmd
    - added ghuser and ghpass to install cmd
    - changed pg-worldlogin description

--- tasks/sys/webmin/install.cmd
+++ tasks/sys/webmin/install.cmd
@@ -1,10 +1,12 @@
-xtuple-server install-$instanceType --xt-name $xtName --xt-version $xtVersion --xt-edition $xtEdition $xtDatabases --xt-maindb $xtMaindb --xt-adminpw $xtAdminpw --nginx-domain $nginxDomain --nginx-inkey $nginxInkey --nginx-incrt $nginxIncrt --pg-slots $pgSlots --pg-capacity $pgCapacity $pgEnableSnapshots $pgWorldLogin $pgSnapSchedule $pgSnapCount $pgPort $pgVersion
+xtuple-server install-$instanceType --xt-name $xtName --xt-version $xtVersion --xt-edition $xtEdition $xtDatabases --xt-ghuser $xtGhuser --xt-ghpass $xtGhpass --xt-maindb $xtMaindb --xt-adminpw $xtAdminpw --nginx-domain $nginxDomain --nginx-inkey $nginxInkey --nginx-incrt $nginxIncrt --pg-slots $pgSlots --pg-capacity $pgCapacity $pgEnableSnapshots $pgWorldLogin $pgSnapSchedule $pgSnapCount $pgPort $pgVersion
 New xTuple Deployment (xtuple-server install)
 root 0 0 0 0 1 3600 0 -
 xtName:0::1,1:Account Name
 xtVersion:0:4.5.0:1,1:xTuple App Version
 instanceType:9:/etc/webmin/xtuple/types.menu:1,1:Instance Type
 xtEdition:9:/etc/webmin/xtuple/editions.menu:1,1:xTuple Edition
+xtGhuser:0::1,0:Github Account Username
+xtGhpass:8::1,0:Github Account Password
 xtDatabases:12:/etc/webmin/xtuple/databases.menu:0,1:Initialize Databases
 xtMaindb:5::1,0:xTuple Main Database File
 xtAdminpw:8::1,0:xTuple "admin" Password
@@ -14,7 +16,7 @@ nginxIncrt:5::1,0:SSL certificate (.crt or .pem)
 pgVersion:9:/etc/webmin/xtuple/pgversions.menu:1,1:Postgres Server Version
 pgCapacity:0:32:1,1:Capacity (slots)
 pgSlots:0:1:1,1:Provision (slots)
-pgWorldLogin:7:--pg-worldlogin:0,0:Expose Postgres to All IPs
+pgWorldLogin:7:--pg-worldlogin:0,0:Allow Postgres Login from Anywhere
 pgPort:0::1,0:Postgres Port
 pgEnableSnapshots:7:--pg-snapenable:0,0:Enable Snapshot Manager?
 pgSnapSchedule:0:@daily:1,1:Snapshot Schedule
