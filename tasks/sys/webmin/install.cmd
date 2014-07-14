caed78c110deadcd6b9028ebd74b6eb62e5621bb
Travis Webb <me@traviswebb.com>
2014-07-14 03:03:52 -0400

    add --xt-authkey option to webmin install cmd

--- tasks/sys/webmin/install.cmd
+++ tasks/sys/webmin/install.cmd
@@ -1,4 +1,4 @@
-xtuple-server install-$instanceType --xt-name $xtName --xt-version $xtVersion --xt-edition $xtEdition $xtDatabases --xt-maindb $xtMaindb --xt-adminpw $xtAdminpw --nginx-domain $nginxDomain --nginx-inkey $nginxInkey --nginx-incrt $nginxIncrt --pg-slots $pgSlots --pg-capacity $pgCapacity $pgEnableSnapshots $pgWorldLogin $pgSnapSchedule $pgSnapCount $pgPort $pgVersion
+xtuple-server install-$instanceType --xt-name $xtName --xt-version $xtVersion --xt-edition $xtEdition $xtDatabases --xt-maindb $xtMaindb --xt-authkey $xtAuthkey --xt-adminpw $xtAdminpw --nginx-domain $nginxDomain --nginx-inkey $nginxInkey --nginx-incrt $nginxIncrt --pg-slots $pgSlots --pg-capacity $pgCapacity $pgEnableSnapshots $pgWorldLogin $pgSnapSchedule $pgSnapCount $pgPort $pgVersion
 New xTuple Deployment (xtuple-server install)
 root 0 0 0 0 1 3600 0 -
 xtName:0::1,1:Account Name
@@ -8,6 +8,7 @@ xtEdition:9:/etc/webmin/xtuple/editions.menu:1,1:xTuple Edition
 xtDatabases:12:/etc/webmin/xtuple/databases.menu:0,1:Initialize Databases
 xtMaindb:5::1,0:xTuple Main Database File
 xtAdminpw:8::1,0:xTuple "admin" Password
+xtAuthkey:0:xTuple:1,0:Enhanced Auth Key
 nginxDomain:0:localhost:1,0:Domain Name
 nginxInkey:5::1,0:SSL key (.key)
 nginxIncrt:5::1,0:SSL certificate (.crt or .pem)
